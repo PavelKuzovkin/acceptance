55176afa715526155a288397981283dff5e7875b
Pavel Kuzovkin <pavel.kuzovkin@intistele.com>
2021-12-03 00:09:12 +0300

    docker

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 docker build -f ./docker/Dockerfile.back --target builder .
 docker build -f ./docker/Dockerfile.back --target backend -t "backend:latest" .
 
+docker build -f ./docker/Dockerfile.front --target builder .
+docker build -f ./docker/Dockerfile.front --target cabinet -t "cabinet:latest" .
+
 docker-compose -f ./docker/docker-compose.yml stop
 docker-compose -f ./docker/docker-compose.yml rm -f
 docker-compose -f ./docker/docker-compose.yml up -d
-
