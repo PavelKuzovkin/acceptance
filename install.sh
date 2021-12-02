7eb561dc1e73cff0c3cb1e3729c327f940b4bc36
Pavel Kuzovkin <pavel.kuzovkin@intistele.com>
2021-12-02 22:48:14 +0300

    docker

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 docker build -f ./docker/Dockerfile.back --target builder .
 docker build -f ./docker/Dockerfile.back --target backend -t "backend:latest" .
+
+docker-compose -f ./docker/docker-compose.yml stop
+docker-compose -f ./docker/docker-compose.yml rm -f
+docker-compose -f ./docker/docker-compose.yml up -d
+
