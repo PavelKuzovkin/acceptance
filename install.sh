f8f55892adde6d6dd9375fb568c348f596a38640
Pavel Kuzovkin <pavel.kuzovkin@intistele.com>
2021-12-04 01:29:05 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,6 @@ docker-compose -f ./docker/docker-compose.yml up -d
 
 mkdir -p /var/www/html
 cp -R -f ./recognition/pepper-cam /var/www/html
+cp -f ./recognition/pepper-cam/group1-shard1of3.bin /var/www/html
+cp -f ./recognition/pepper-cam/group1-shard2of3.bin /var/www/html
+cp -f ./recognition/pepper-cam/group1-shard3of3.bin /var/www/html
